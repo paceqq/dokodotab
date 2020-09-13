95545f616faea6b894f42285272b482ca0974407
Volksfest <s.barut@tu-bs.de>
2020-09-13 13:18:56 +0200

    Add some features:
    Command Add replaced by default
    Command Solo integrated into defaul (one player name is assumed as Solo)
    Insert try catch between asynch message handling per client (thread) and per client arrival (main)
    Sanitize Name (one word, no spaces)
    Set domain and file by console args
    Beautify outputs a bit more (still improvable)

--- doko-sharp/EntryPoint.cs
+++ doko-sharp/EntryPoint.cs
@@ -17,9 +17,17 @@ namespace doko
     public class EntryPoint
     {
 
-        static void Main()
+        static void Main(string[]args)
         {
-            runWebsocket();
+            string domain = "localhost";
+            string uri = @"G:\GIT\dokodotab\doko-sharp\Test.html";
+            if (args.Length > 0) {
+                uri = args[0];
+            }
+            if (args.Length > 1) {
+                domain = args[1];
+            }
+            runWebsocket(domain, uri);
         }
 
         static async void startWSHandler(object context_raw) {
@@ -45,88 +53,137 @@ namespace doko
             // actual processing
             while (true)
             {
-                var wsresult = await ws.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffy), Token);
-                
-                if (wsresult.MessageType != WebSocketMessageType.Close)
+                try
                 {
-                    string cmd = text_decoder.GetString(buffy, 0, wsresult.Count);
-                    System.Console.Write("> ");
-                    System.Console.WriteLine(cmd);
+                    var wsresult = await ws.WebSocket.ReceiveAsync(new ArraySegment<byte>(buffy), Token);
 
-                    stringBuilder.Clear();
-                    parser.Parse(cmd);
-                    
-                    if (stringBuilder.Length != 0)
+                    if (wsresult.MessageType != WebSocketMessageType.Close)
                     {
-                        System.Console.WriteLine(stringBuilder.ToString());
-                        await ws.WebSocket.SendAsync(text_decoder.GetBytes(stringBuilder.ToString()), WebSocketMessageType.Text, true, Token);
+                        string cmd = text_decoder.GetString(buffy, 0, wsresult.Count);
+                        System.Console.Write("> ");
+                        System.Console.WriteLine(cmd);
+
+                        stringBuilder.Clear();
+                        parser.Parse(cmd);
+
+                        if (stringBuilder.Length != 0)
+                        {
+                            System.Console.WriteLine(stringBuilder.ToString());
+                            await ws.WebSocket.SendAsync(text_decoder.GetBytes(stringBuilder.ToString()), WebSocketMessageType.Text, true, Token);
+                        }
                     }
-                }
-                else //else close
+                    else //else close
+                    {
+                        ws.WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye", Token).Wait();
+                        Console.WriteLine("Closed WebSocket");
+                        return;
+                    }
+                } catch
                 {
-                    ws.WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye", Token).Wait();
-                    Console.WriteLine("Closed WebSocket");
-                    return;
+                    Console.WriteLine("Unhandled Exception with WebSocket");
+                    break;
                 }
             }
-            
-            
         }
 
-        static void runWebsocket() {            
+        static void runWebsocket(string domain, string uri) {            
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost/");
+            listener.Prefixes.Add("http://"+ domain + "/");
             listener.Start();
             Console.WriteLine("Listening...");
 
             bool run = true;
             while (run)
             {
-                // Wait for Request
-                HttpListenerContext context = listener.GetContext();
-                HttpListenerRequest request = context.Request;
-
-                if (request.RawUrl.Equals("/Exit"))
+                try
                 {
-                    run = false;
+                    // Wait for Request
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+
+                    if (request.RawUrl.Equals("/Exit"))
+                    {
+                        run = false;
+                    }
+
+                    if (request.IsWebSocketRequest)
+                    {
+                        var thread = new Thread(startWSHandler);
+                        thread.Start(context);
+                        continue;
+                    }
+
+                    HttpListenerResponse response = context.Response;
+
+                    // Construct a response.
+                    string responseString = File.ReadAllText(uri).Replace("<!--DOMAIN-->",domain);
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                    // Get a response stream and write the response to it.
+                    response.ContentLength64 = buffer.Length;
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    // You must close the output stream.
+                    output.Close();
+
+                    Console.WriteLine("Served client");
                 }
-
-                if (request.IsWebSocketRequest)
-                {
-                    var thread = new Thread(startWSHandler);
-                    thread.Start(context);
-                    continue;
+                catch {
+                    Console.WriteLine("Got weird exception while handling client");
                 }
-
-                HttpListenerResponse response = context.Response;
-
-                // Construct a response.
-                string responseString = File.ReadAllText(@"G:\GIT\dokodotab\doko-sharp\Test.html");
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-                // Get a response stream and write the response to it.
-                response.ContentLength64 = buffer.Length;
-                System.IO.Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                // You must close the output stream.
-                output.Close();
-
-                Console.WriteLine("Served client");
             }
             listener.Stop();
         }
 
         static CommandParser createParser(Session session, List<Player> player, StringBuilder builder) {
             CommandParser parser = new CommandParser();
-            
+
+            /*
             parser.BindFunction(
                 "Add", (str) => {
-                    player.Add(new Player(String.Join(' ', str)));
+                    if (str.Length != 1)
+                    {
+                        builder.AppendLine("Leerzeichen im Namen nicht erlaubt!");
+                        return null;
+                    }
+                    else
+                    {
+                        var name = str[0].Trim();
+                        if (String.IsNullOrEmpty(name)) {
+                            builder.AppendLine("Name wird benötigt.");
+                            return null;
+                        }
+                        player.Add(new Player(name));
+                        builder.AppendLine("Spieler " + name + " wurde hinzugefügt.");
+                    }
+                    return null;
+                },
+                State.CreateParty
+            );*/
+            parser.BindDefault(
+                (str) =>
+                {
+                    if (str.Length != 1)
+                    {
+                        builder.AppendLine("Leerzeichen im Namen nicht erlaubt!");
+                        return null;
+                    }
+                    else
+                    {
+                        var name = str[0].Trim();
+                        if (String.IsNullOrEmpty(name))
+                        {
+                            builder.AppendLine("Name wird benötigt.");
+                            return null;
+                        }
+                        player.Add(new Player(name));
+                        builder.AppendLine("Spieler " + name + " wurde hinzugefügt.");
+                    }
                     return null;
                 },
                 State.CreateParty
             );
-
+            
             parser.BindFunction(
                 "List", (str) => {
                     foreach (var player in player)
@@ -169,80 +226,12 @@ namespace doko
                 State.RunSession
             );
 
-            parser.BindFunction(
-                "Solo", (str) => {
-
-                    // Defaultvalues
-                    bool lost = false;
-                    bool bock = false;
-
-                    // Needs to be checked if set
-                    String playerName = null;
-                    int points = -1;
-
-                    foreach (var arg in str)
-                    {
-                        if (arg.Equals("Lost"))
-                        {
-                            lost = true;
-                        }
-                        else if (arg.Equals("Bock"))
-                        {
-                            bock = true;
-                        }
-                        else if (session.ActivePlayers.HasPlayer(arg) && playerName is null)
-                        {
-                            playerName = arg;
-                        }
-                        else
-                        {
-                            try
-                            {
-                                points = Int32.Parse(arg);
-                            }
-                            catch
-                            {
-                                builder.AppendLine("Parametereingabe ist falsch!" + " (" + arg + ")");
-                                builder.AppendLine("Parameter muss eine Punktezahl, den Solo-Spieler und optional \"Bock\" oder \"Lost\" angeben.");
-
-                                builder.AppendLine("Es spielen: " + session.ActivePlayerString);
-                                return null;
-                            }
-                        }
-                    }
-
-                    if (points == -1)
-                    {
-                        builder.AppendLine("Spielwert muss angegeben werden!");
-                        return null;
-                    }
-
-                    if (playerName is null)
-                    {
-                        builder.AppendLine("Solo Spieler muss angegeben werden!");
-                        return null;
-                    }
-
-                    if (lost)
-                    {
-                        String[] winningPlayersName = (from player in session.ActivePlayers.Invert(new Player[] { new Player(playerName) }) select player.Name).ToArray();
-                        session.AddSolo(points, bock, winningPlayersName);
-                    }
-                    else
-                    {
-                        session.AddSolo(points, bock, playerName);
-                    }
-                    builder.Append(session.GetLastGameString());
-                    return null;
-                },
-                State.RunSession
-            );
-
             parser.BindDefault(
                 (str) =>
                 {
                     // default value
                     bool bock = false;
+                    bool lost = false; // if player count eq 1
 
                     // needs to be checked if set
                     List<String> players = new List<String>();
@@ -254,8 +243,21 @@ namespace doko
                         {
                             bock = true;
                         }
-                        else if (session.ActivePlayers.HasPlayer(arg) && players.Count < 2 && (players.Count != 1 || !players[0].Equals(arg))) // Last Expression is an =>
+                        else if (arg.Equals("Lost"))
                         {
+                            lost = true;
+                        }
+                        else if (session.ActivePlayers.HasPlayer(arg))
+                        {
+                            if (player.Count == 2) {
+                                builder.AppendLine("Es können maximal nur 2 Spieler angegeben werden!\nFür verlorene Solo-Runde entsprechend den Solo Spieler mit \"Lost\" eingeben");
+                                return null;
+                            }
+                            if (players.Count == 1 && players[0].Equals(arg)) 
+                            {
+                                builder.AppendLine("Ein Spieler kann nur einmal angegeben werden!");
+                                return null;
+                            }
                             players.Add(arg);
                         }
                         else
@@ -266,9 +268,8 @@ namespace doko
                             }
                             catch
                             {
-                                builder.AppendLine("Parametereingabe ist falsch!" + " (" + arg + ")");
-                                builder.AppendLine("Parameter muss eine Punktezahl, zwei aktive Spieler und optional \"Bock\" angeben.");
-                                builder.AppendLine("Es spielen: " + session.ActivePlayerString);
+                                builder.AppendLine("Parametereingabe kann nicht verwertet werden!" + " (" + arg + ")");
+                                builder.AppendLine("\nEs spielen: " + session.ActivePlayerString);
                                 return null;
                             }
                         }
@@ -280,13 +281,30 @@ namespace doko
                         return null;
                     }
 
-                    if (players.Count != 2)
+                    if (players.Count == 2)
                     {
-                        builder.AppendLine("Es müssen genau 2 Gewinner angegeben werden!");
+                        if (lost) {
+                            builder.AppendLine("\"Lost\" Parameter wird ignoriert!");
+                        }
+                        session.AddStandard(points, bock, players.ToArray());
+                        builder.AppendLine("Standardspiel gewonnen von " + String.Join(" ", players.ToArray()));
+                    }
+                    else if (players.Count == 1)
+                    {
+                        String[] winningPlayers = players.ToArray();
+                        if (lost) {
+                            var soloPlayer = new Player[] { new Player(players.First()) };
+                            winningPlayers = (from player in session.ActivePlayers.Invert(soloPlayer) select player.Name).ToArray();
+                        }
+                        session.AddSolo(points, bock, winningPlayers);
+                        builder.AppendLine("Solospiel gewonnen von " + String.Join(" ", winningPlayers));
+                    }
+                    else 
+                    {
+                        builder.AppendLine("Genau ein Spieler eingeben für ein Solo.\nZwei Spieler für ein gewöhnliches Spiel.\nAlles andere ist falsch!");
                         return null;
                     }
 
-                    session.AddStandard(points, bock, players.ToArray());
                     builder.Append(session.GetLastGameString());
                     return null;
                 },
